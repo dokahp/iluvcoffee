4a8949bdfcd4f8edd4068eb4880e6c3032aa1eda
Vitaliy Dreko <vitali.dreko@gmail.com>
2023-01-09 10:15:45 +0300

    feat: add validation pipe object, that forbidds no dto key-value pairs

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// whitelist: true - удаляет из запроса все свойства, которые не соответствуют dto
+// forbidNonWhitelisted - дает bad request error, если endpoint получает свойство, которое не ожидает
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
