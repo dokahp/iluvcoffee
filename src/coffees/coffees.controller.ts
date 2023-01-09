135f176a01acd1549e2b90425cf7d4ff04c0bcb4
Vitaliy Dreko <vitali.dreko@gmail.com>
2023-01-09 07:14:15 +0300

    feat: add nestjs/swagger for mapped-types, add validate input data with dto

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -19,7 +19,6 @@ export class CoffeesController {
   }
 
   @Post()
-  @HttpCode(HttpStatus.GONE)
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
     return this.coffeesService.create(createCoffeeDto);
   }
