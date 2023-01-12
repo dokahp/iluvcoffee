08887dab8cee95af8122a6a05aba54bcb9767eae
Vitaliy Dreko <vitali.dreko@gmail.com>
2023-01-12 16:51:57 +0300

    feat: add cascading inserts and updates

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -4,22 +4,28 @@ import { Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
-
+import { Flavor } from './entities/flavor.entity';
 
 @Injectable()
 export class CoffeesService {
-  
   constructor(
     @InjectRepository(Coffee)
-    private readonly coffeeRepository: Repository<Coffee>
+    private readonly coffeeRepository: Repository<Coffee>,
+    @InjectRepository(Flavor)
+    private readonly flavorRepository: Repository<Flavor>,
   ) {}
 
   async findAll() {
-    return await this.coffeeRepository.find();
+    return await this.coffeeRepository.find({
+      relations: ['flavors'],
+    });
   }
 
   async findOne(id: string) {
-    const coffee = await this.coffeeRepository.findOne({where: {id: +id}});
+    const coffee = await this.coffeeRepository.findOne({
+      where: { id: +id },
+      relations: ['flavors'],
+    });
     if (!coffee) {
       // throw new HttpException(`Coffee with id=${id} not found`, HttpStatus.NOT_FOUND);
       throw new NotFoundException(`Coffee with id=${id} not found`);
@@ -27,24 +33,47 @@ export class CoffeesService {
     return coffee;
   }
 
-  create(createcoffeeDto: CreateCoffeeDto) {
-    const coffee =  this.coffeeRepository.create(createcoffeeDto);
+  async create(createcoffeeDto: CreateCoffeeDto) {
+    const flavors = await Promise.all(
+      createcoffeeDto.flavors.map((flavor) => this.preloadFlavorByName(flavor)),
+    );
+    const coffee = this.coffeeRepository.create({
+      ...createcoffeeDto,
+      flavors,
+    });
     return this.coffeeRepository.save(coffee);
   }
 
   async update(id: string, updateCoffeeDto: UpdateCoffeeDto) {
+    // так как массива вкусов может не быть при обновлении делаем проверку
+    const flavors = updateCoffeeDto.flavors && (
+      await Promise.all(updateCoffeeDto.flavors.map(flavor => this.preloadFlavorByName(flavor)))
+    );
     const coffee = await this.coffeeRepository.preload({
       id: +id,
-      ...updateCoffeeDto
-    })
+      ...updateCoffeeDto,
+      flavors
+    });
     if (!coffee) {
-      throw new NotFoundException(`Coffee with id=${id} not found`)
+      throw new NotFoundException(`Coffee with id=${id} not found`);
     }
     return this.coffeeRepository.save(coffee);
   }
 
   async remove(id: string) {
-    const coffee = await this.coffeeRepository.findOne({where: {id: +id}});
+    const coffee = await this.coffeeRepository.findOne({ where: { id: +id } });
     return this.coffeeRepository.remove(coffee);
+  }
+
+  private async preloadFlavorByName(name: string): Promise<Flavor> {
+    const existingFlavor = await this.flavorRepository.findOne({
+      where: {
+        name: name,
+      },
+    });
+    if (existingFlavor) {
+      return existingFlavor;
+    }
+    return this.flavorRepository.create({ name });
   }
 }
